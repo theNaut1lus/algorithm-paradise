813e3fe96b194d4e6519241d35d0f0e5789d9416
CO18325 <42858969+CO18325@users.noreply.github.com>
2019-10-21 15:17:15 +0530

    Create cap-greatest.js

---
+++ entries/cap-greatest.js
@@ -0,0 +1,20 @@
+console.log("loaded");
+
+function swap() {
+    console.log("called");
+    var a = document.getElementById("value-a").value;
+    var b = document.getElementById("value-b").value;
+    console.log(a);
+    console.log(b);
+
+  
+    var largest;
+    if(a>b)
+      largest=a;
+    else
+      largest=b;
+    var string = "Maximum value is equal to " + largest;
+
+    document.getElementById("return-value").innerHTML = string;
+    console.log(string);
+}

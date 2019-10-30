86b6fb83b246d95d19f298f399beea37cb75e3dd
udaykumarjangra <42859049+udaykumarjangra@users.noreply.github.com>
2019-10-30 21:27:14 +0530

    create circular_queue.c
    Implementing Circular Queue in c

---
+++ entries/circular_queue.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAXSIZE 5
+
+typedef struct que{
+	int que_arr[MAXSIZE];
+	int rear;
+	int front;	
+} QUEUE;
+QUEUE q;
+
+
+void insert(){
+	int no;
+	if( (q.front ==q.rear + 1) || (q.front == 0 && q.rear == MAXSIZE-1))
+	{	printf("Front %d\n",q.front);
+		printf("Last %d\n",q.rear);
+		printf("Queue Overflow, Insertion not possible\n");
+	}
+	else {
+		if(q.front==-1)
+		{
+			q.front=0;
+		}
+		printf("Front %d\n",q.front);
+		printf("Last %d\n",q.rear);
+		printf("Enter number you want to insert in the queue\n");
+		scanf("%d",&no);
+		q.rear=(q.rear+1)%MAXSIZE;
+		q.que_arr[q.rear]=no;
+		printf("Number Inserted\n");
+	}
+	getch();
+	system("cls");
+}
+
+void dele()
+{	int no;
+	if(q.front==-1)
+	{
+		printf("Queue is Empty, Deletion not possible\n");
+		
+	}
+	else{
+		if(q.front==q.rear)
+		{
+			q.front=-1;
+			q.rear=-1;
+		}
+		else{
+		no=q.que_arr[q.front];
+		q.front=(q.front+1)%MAXSIZE;
+		printf("Number %d deleted from queue",no);
+		}
+	}
+	getch();
+	system("cls");
+}
+
+void display()
+{	int i;
+	if(q.front==-1)
+	{
+		printf("Queue is Empty\n");
+		
+	}
+	else{
+		for(i=q.front;i!=q.rear;i=(i+1)%MAXSIZE)
+		{
+			printf("%d ",q.que_arr[i]);
+		}
+		printf("%d",q.que_arr[i]);
+	}
+	getch();
+	system("cls");
+}
+int main()
+{	q.front=-1;
+	q.rear=-1;
+	int choice,no;
+	while(1){
+		printf("Circular Queue using array\n");
+		printf("1.Insertion\n2.Deletion\n3.Display\n4.Exit\n");
+		scanf("%d",&choice);
+		switch(choice){
+			case 1: insert();
+					break;
+			case 2: dele();
+					break;
+			case 3: display();
+					break;
+			case 4: exit(0);
+					break;
+		}
+	}
+	
+}
